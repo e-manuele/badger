b29117c157e0c15a58fa508592d8e28989fcbff7
E-manuele <74198922+e-manuele@users.noreply.github.com>
2023-10-27 10:39:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,35 @@ led_green.value(0)
 led_red.value(0)
 led_blue.value(0)
 reader = MFRC522(spi_id=0,sck=2,mosi=3,miso=4,cs=5,rst=0)
-
+#2798319075
 def toggle(obj):
         obj.value(1)
         time.sleep(0.5)
         obj.value(0)
 print("OK")
-reader.init()
+
 while True:
-    # read a command from the host
-    v = sys.stdin.readline().strip()
-    if v.lower() == "on":
-        led_blue.value(1)
-        time.sleep(1)
-        print("...attiva")
-    elif v.lower() == "ok":
-        toggle(led_green)
-    elif v.lower()=="no":
-        toggle(led_red)
-    else:
-        print("Not found")
-############################################################        
-    (status, tag_type) = self.reader.request(self.reader.REQIDL)
-    if status == self.reader.OK:
-       print("Scheda rilevata")
-       (status, uid) = self.reader.anticoll()
-       if status == self.reader.OK:
-          print("UID della scheda:", uid)
+        
+        # read a command from the host
+        v = sys.stdin.readline().strip()
+        if v.lower() == ".":
+            toggle(led_blue)
+        elif v.lower() == "..on":
+            print("...attiva")
+        elif v.lower() == "ok":
+            toggle(led_green)
+        elif v.lower()=="no":
+            toggle(led_red)
+        else:
+            print("Not found")
+        
+        # read a card from the rfid module
+        reader.init()
+        (stat, tag_type) = reader.request(reader.REQIDL)
+        if stat == reader.OK:
+            (stat, uid) = reader.SelectTagSN()
+            if stat == reader.OK:
+                card = int.from_bytes(bytes(uid),"little",False)
+                print(str(card))
+        time.sleep(1) 
+
